2154d712face22bc61f82ef18d8f37e9b968f43f
Alvaro Miranda <kikitux@desktop.home.kikitux.net>
2016-03-07 01:25:27 +1300

    add packer

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Simple scripts that download hashicorp applications
 
+## For ruby `<file.rb>` packages
+
+It requires `ruby` and `rubyzip` gem
+
+For RH and friends: `yum install -y ruby rubygems && gem install rubyzip`
+
 ###Note
 These scripts at the moment skip -rc version.
 The reason behind this, is if you are faster testing a rc version, or building for master, you probably won't need these.
---
+++ download-packer.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/env ruby
+
+product="packer"
+binaryfile="packer"
+os="linux"                                                                                                                                                                                                                   
+arch="amd64"
+require "rubygems/package"
+require "json"
+require "net/http"
+require "open-uri"
+require "zip"
+
+uri = URI.parse("https://releases.hashicorp.com/#{product}/index.json")
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+request = Net::HTTP::Get.new(uri.request_uri)
+response = http.request(request)
+
+if response.code == "200"                                                                                                                                                                                                    
+  result = JSON.parse(response.body)                                                                                                                                                                                         
+  versions = result["versions"]                                                                                                                                                                                              
+  maxver = "0.0.0"                                                                                                                                                                                                           
+  versions.each do |key, value|                                                                                                                                                                                              
+    if versions[key]["version"].include? "rc"                                                                                                                                                                                
+      maxver = maxver                                                                                                                                                                                                        
+    elsif Gem::Version.new(maxver) < Gem::Version.new(versions[key]["version"])                                                                                                                                              
+      maxver = versions[key]["version"]                                                                                                                                                                                      
+    end                                                                                                                                                                                                                      
+  end                                                                                                                                                                                                                       
+  thisone = versions[maxver]["builds"].select { |builds| builds["os"] == os && builds["arch"] == arch}                                                                                                                       
+  url = thisone[0]["url"]                                                                                                                                                                                                    
+  filename = thisone[0]["filename"]
+  
+  if File.file? filename                                                                                                                                                                                                     
+    puts "file #{filename} present"                                                                                                                                                                                          
+  else                                                                                                                                                                                                                       
+    open(filename, 'wb') do |file|                                                                                                                                                                                           
+      file << open(url).read                                                                                                                                                                                                 
+    end                                                                                                                                                                                                                      
+  end
+  
+  file = binaryfile
+  
+  Zip::File.open(filename) do |zip|                                                                                                                                                                                          
+    zip.each do |entry|                                                                                                                                                                                                      
+      if entry.file? and entry.name == file                                                                                                                                                                                  
+        File.open file, "wb" do |f|                                                                                                                                                                                          
+          f.print entry.get_input_stream.read                                                                                                                                                                                
+        end                                                                                                                                                                                                                  
+        FileUtils.chmod 0755, file                                                                                                                                                                                           
+      end                                                                                                                                                                                                                    
+    end                                                                                                                                                                                                                      
+  end
+  
+  FileUtils.rm filename if File.exists?(filename)                                                                                                                                                                            
+  else
+  puts "check product #{product} is valid"                                                                                                                                                                                   
+end 
